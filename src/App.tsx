dcabd3ec4d89b0e6d0b9978ce44132dd8a99a993
lgklsv <kolosov20011@gmail.com>
2023-06-02 12:40:01 +0300

    feat: add styled components and test it out

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,14 @@
+import styled from 'styled-components';
+
+const StyledButton = styled.button`
+  color: white;
+  padding: 15px 32px;
+  text-align: center;
+  background-color: #4caf50;
+`;
+
 function App() {
-  return <h1>hello world</h1>;
+  return <StyledButton type="button">cal</StyledButton>;
 }
 
 export default App;
